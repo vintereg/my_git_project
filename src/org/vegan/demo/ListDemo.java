ff8a11eea41a66a4c7213263a3a5638346f02aeb
vintereg <vintereg@users.noreply.github.com>
2016-03-13 22:33:27 -0400

    add java source code

---
+++ src/org/vegan/demo/ListDemo.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package org.vegan.demo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author egan
+ *
+ */
+public class ListDemo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+
+		List<String> lstValues = new ArrayList<>();
+		
+		lstValues.add("Larry");
+		
+		System.out.println("Size of the array is:  " + lstValues.size());
+		
+		
+
+	}
+
+}
